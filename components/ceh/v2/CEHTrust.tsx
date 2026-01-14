5bd0584e47de7b8fea19ddd56e34635b2adf17c1
DCGit208 <bthedugrp@gmail.com>
2026-01-14 14:57:16 +0100

    Fix missing FaAward import in CEHTrust component

--- components/ceh/v2/CEHTrust.tsx
+++ components/ceh/v2/CEHTrust.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import { FaQuoteLeft, FaStar, FaBuilding } from 'react-icons/fa'
+import { FaQuoteLeft, FaStar, FaBuilding, FaAward } from 'react-icons/fa'
 
 const CEHTrust = () => {
   // TODO: Replace with real testimonials when available
